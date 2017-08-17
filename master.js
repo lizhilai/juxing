313ba6bea360e0104b39fb52df0e438195ad52ce
lizhilai <1307393510@qq.com>
2017-08-17 11:28:06 +0800

    init

---
+++ master.js
@@ -0,0 +1,61 @@
+// console.log('run..')
+var removeClass = function(element, classname) {
+    element.classList.remove(classname)
+}
+var addClass = function(element, classname) {
+    element.classList.add(classname)
+}
+var clearClass = function(classname) {
+    var selector = '.' + classname
+    var elements = document.querySelectorAll(selector)
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i]
+        removeClass(element, classname)
+    }
+}
+var bindIndexOne = function() {
+    var elements = document.querySelectorAll('.item1')
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i]
+        element.addEventListener('click', function(event) {
+            var target = event.target
+            clearClass('active-one')
+            addClass(target, 'active-one')
+        })
+    }
+}
+var bindIndexTwo = function() {
+    var elements = document.querySelectorAll('.item2')
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i]
+        element.addEventListener('click', function(event) {
+            var target = event.target
+            clearClass('active-two')
+            addClass(target, 'active-two')
+        })
+    }
+}
+var bindIndexThree = function() {
+    var elements = document.querySelectorAll('.item3')
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i]
+        element.addEventListener('click', function(event) {
+            var target = event.target
+            clearClass('active-three')
+            addClass(target, 'active-three')
+        })
+    }
+}
+var toggleAside = function() {
+    var leftSide = document.querySelector('.left-side')
+    var rightSide = document.querySelector('.right-side')
+    toggleClass(leftSide, 'active-left')
+    toggleClass(rightSide, 'active-right')
+}
+
+var __app = function() {
+    bindIndexOne()
+    bindIndexTwo()
+    bindIndexThree()
+}
+__app()
